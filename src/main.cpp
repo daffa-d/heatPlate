e7028d81d312c2b7e40bbd5eab961d514d2cc145
2six0 <daffa123destrata@gmail.com>
2022-11-09 15:56:53 +0700

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,23 @@ float temp;
 float PID_P, PID_I, PID_D;
 float PID_ERROR, PREV_ERROR;
 int setPoint = 0;
-int set = 120;
-float kP = 0.1, kI = 0.01, kD = 0.01;
+int set = 150;
+float kP = 2, kI = 0.0025, kD = 9;
 float PID_OUTPUT;
+float PID_OUTPUT_MAX = 180;
+float PID_OUTPUT_MIN = 0;
 float pwmVal = 255;
 int mode = 0;
 int preheat = 70;
 int detik;
+int pTime, eTime;
+float refreshRate = 500;
+bool state = false;
 
 void pidController();
 void lcdPrint(int x, int y, String text);
 void suhu();
+void time();
 
 void setup()
 {
@@ -40,34 +46,42 @@ void setup()
 
 void loop()
 {
+  time();
   suhu();
   if (mode == 0)
   {
     lcdPrint(0, 0, "T: " + String(temp) + "C");
     lcdPrint(0, 1, "S: " + String(set));
     if (!digitalRead(2))
-    {
+    { 
+      while(!digitalRead(2)){}
       set += 10;
       delay(10);
-      if (set > 300)
+      if (set > 200)
       {
-        set = 300;
+        set = 200;
       }
     }
     else if (!digitalRead(3))
     {
+      while(!digitalRead(3)){}
       set -= 10;
       delay(10);
-      if (set < 120)
+      if (set < 150)
       {
-        set = 120;
+        set = 150;
       }
     }
     else if (!digitalRead(5))
-    {
+    { 
+      while(!digitalRead(5)){}
+      detik = 0;
       mode = 1;
       lcd.clear();
       delay(10);
+    }
+    if(detik > 2000){
+      detik = 0;
     }
   }
 
@@ -75,57 +89,83 @@ void loop()
   {
     lcdPrint(0, 0, "T: " + String(temp) + "C");
     lcdPrint(2, 1, "Preheat Dulu");
-    setPoint = 200;
-    if (temp > 100 && temp < 120)
+    setPoint = detik * 1.556; // 140
+    if (temp > 140 && detik < 2500)
     {
       lcdPrint(2, 1, "Preheat Selesai");
-      delay(3000);
+      delay(2000);
       lcd.clear();
       mode = 2;
     }
+    state = true;
   }
 
   else if (mode == 2)
   {
-    lcdPrint(0,0, "T: " + String(temp) + "C");
-    lcdPrint(0,1, "Set to SetPoint");
+    lcdPrint(0, 0, "T: " + String(temp) + "C");
+    lcdPrint(0, 1, "Set to SetPoint");
     setPoint = set;
-    if(temp > setPoint - 5 && temp < setPoint + 5){
-      lcdPrint(2,1, "SetPoint Reached");
-      delay(3000);
-      lcd.clear();
-      mode = 3;
+    if (temp > setPoint - 2 && temp < setPoint + 2)
+    {
+        lcdPrint(2, 1, "SetPoint !!!");
+        delay(2000);
+        detik = 0;
+        lcd.clear();
+        mode = 3;  
     }
+    else
+    {
+      state = true;
+    }
+  } else if (mode == 3 ){
+    state = true;
+    lcdPrint(0, 0, "T: " + String(temp) + "C");
+    lcdPrint(2,1, "Peak SetPoint");
+    if(detik > 10 && detik < 2000) {
+      setPoint = set;
+      
+    } else if(detik > 2000){
+        state = false;
+        lcdPrint(2,1, "Cooling");
+    } 
+    
   }
-  pidController();
+
+  if (state)
+  {
+    pidController();
+  }
+  else
+  {
+    analogWrite(10, 0);
+    analogWrite(9, 0);
+  }
 }
 
 void pidController()
-{
-  if (mode == 0)
-  {
-    setPoint = 255;
-  } else { 
-    PID_ERROR = setPoint - temp;
-    PID_P = kP * PID_ERROR;
-    PID_I = PID_I + (kI * PID_ERROR);
-    PID_D = kD * (PID_ERROR - PREV_ERROR);
-    PID_OUTPUT = PID_P + PID_I + PID_D;
-    pwmVal = 255 - PID_OUTPUT;
+{ 
+  float PID_RefreshRate = 50;
+  unsigned long prevTime_PID = 0;
+  if(millis() - prevTime_PID >= PID_RefreshRate){
+  PID_ERROR = setPoint - temp;
+  PID_P = kP * PID_ERROR;
+  PID_I = PID_I + (kI * PID_ERROR);
+  PID_D = kD * (PID_ERROR - PREV_ERROR);
+  PID_OUTPUT = PID_P + PID_I + PID_D;
 
-    if (pwmVal <= 0)
-    {
-      pwmVal = 0;
-    }
-    else if (pwmVal >= 255)
-    {
-      pwmVal = 255;
-    }
-    Serial.println(pwmVal);
-    analogWrite(10, pwmVal);
-    analogWrite(9, pwmVal);
-    PREV_ERROR = PID_ERROR;
-    delay(10);
+  if (PID_OUTPUT > PID_OUTPUT_MAX)
+  {
+    PID_OUTPUT = PID_OUTPUT_MAX;
+  }
+  else if (PID_OUTPUT < PID_OUTPUT_MIN)
+  {
+    PID_OUTPUT = PID_OUTPUT_MIN;
+  }
+  pwmVal = 255 - PID_OUTPUT;
+  // Serial.println(pwmVal);
+  analogWrite(10, pwmVal);
+  analogWrite(9, pwmVal);
+  PREV_ERROR = PID_ERROR;
   }
 }
 
@@ -143,4 +183,15 @@ void suhu()
   {
     temp = therm.analog2temp();
   }
+}
+
+void time()
+{
+  unsigned long prevTime = 0;
+  if (millis() - prevTime > 1000)
+  {
+    prevTime = millis();
+    detik++;
+  }
+  Serial.println(detik);
 }
