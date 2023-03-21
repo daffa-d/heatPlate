9f4f969809b18dfd2ecbfd6cbc97ef0c8c1d6272
Daffa Destrata <46610688+daffa-d@users.noreply.github.com>
2023-03-21 13:13:33 +0700

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,183 +1,234 @@
 #include <Arduino.h>
-#include <thermistor.h>
 #include <LiquidCrystal_I2C.h>
+#include <Heat.h>
+#include <SPI.h>
+#include <DMD2.h>
+#include <TimerOne.h>
+#include <fonts/Arial_Black_16.h>
 
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-thermistor therm(A0, 0);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+heat plate(2, 0.0025, 9, A0);
+SoftDMD dmd(3, 1);
+DMD_TextBox box(dmd);
 
-float temp;
-float PID_P, PID_I, PID_D;
-float PID_ERROR, PREV_ERROR;
-int setPoint = 0;
+float setPoint = 0;
 int set = 150;
-float kP = 2, kI = 0.0025, kD = 9;
-float PID_OUTPUT;
-float PID_OUTPUT_MAX = 220;
-float PID_OUTPUT_MIN = 0;
-float pwmVal = 255;
-int mode = 0;
-int preheat = 70;
 int detik;
-int pTime, eTime;
-float refreshRate = 500;
+int mode = 0;
+int modePlay = 0;
+float temp;
 bool state = false;
 
-void pidController();
 void lcdPrint(int x, int y, String text);
-void suhu();
 void time();
+void clearLCDLine(int line);
+
+const char *MESSAGE = " EEPROM FINISHHH !!!!! ";
+
+// void ScanDMD()
+// {
+//   dmd.scanDisplayBySPI();
+// }
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
-  for (int i = 2; i < 6; i++)
-  {
-    pinMode(i, INPUT_PULLUP);
-  }
+  plate.begin(3, 5, 50);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(10, INPUT_PULLUP);
+  pinMode(12, INPUT_PULLUP);
   lcd.init();
   lcd.backlight();
   lcdPrint(2, 0, "Heater Plate");
   lcdPrint(5, 1, "Ready");
   delay(2000);
   lcd.clear();
+
+  // Timer1.initialize( 5000 );           //period in microseconds to call ScanDMD. Anything longer than 5000 (5ms) and you can see flicker.
+  // Timer1.attachInterrupt( ScanDMD );
+  dmd.begin();
+  dmd.setBrightness(75);
+  dmd.selectFont(Arial_Black_16);
+  dmd.clearScreen();
 }
 
 void loop()
 {
-  time();
-  suhu();
-  if (mode == 0)
+  if (!digitalRead(10))
   {
-    lcdPrint(0, 0, "T: " + String(temp) + "C");
-    lcdPrint(0, 1, "S: " + String(set));
-    if (!digitalRead(2))
-    { 
-      while(!digitalRead(2)){}
-      set += 10;
-      delay(10);
-      if (set > 200)
-      {
-        set = 200;
-      }
-    }
-    else if (!digitalRead(3))
+    while (!digitalRead(10))
     {
-      while(!digitalRead(3)){}
-      set -= 10;
-      delay(10);
-      if (set < 150)
-      {
-        set = 150;
-      }
     }
-    else if (!digitalRead(5))
-    { 
-      while(!digitalRead(5)){}
-      detik = 0;
-      mode = 1;
-      lcd.clear();
-      delay(10);
-    }
-    if(detik > 2000){
-      detik = 0;
+    modePlay++;
+    clearLCDLine(1);
+    if (modePlay > 2)
+    {
+      modePlay = 0;
     }
   }
 
-  else if (mode == 1)
+  if (modePlay == 0)
   {
-    lcdPrint(0, 0, "T: " + String(temp) + "C");
-    lcdPrint(2, 1, "Preheat Dulu");
-    setPoint = detik * 1.556; // 140
-    if (temp > 140 && detik < 2500)
+    lcd.clear();
+    // dmd.clearScreen();
+    // PANEL P10
+    const char *next = MESSAGE;
+    while (*next)
     {
-      lcdPrint(1, 1, "Preheat Selesai");
-      delay(2000);
-      lcd.clear();
-      mode = 2;
+      box.print(*next);
+      next++;
+      if (!digitalRead(10))
+      {
+        break;
+      }
     }
-    state = true;
   }
 
-  else if (mode == 2)
+  else if (modePlay == 1)
   {
-    lcdPrint(0, 0, "T: " + String(temp) + "C");
-    lcdPrint(0, 1, "Set to SetPoint");
-    setPoint = set;
-    if (temp > setPoint - 2 && temp < setPoint + 2)
+    dmd.clearScreen();
+    time();
+    plate.readTemp(temp);
+    if (mode == 0)
     {
-        lcdPrint(0, 1, "  SetPoint !!!  ");
-        delay(1000);
+      dmd.drawString(0, 0, String(temp) + " C");
+      lcdPrint(0, 0, "T: " + String(temp) + "C");
+      lcdPrint(0, 1, "S: " + String(set));
+      if (!digitalRead(2))
+      {
+        while (!digitalRead(2))
+        {
+        }
+        set += 10;
+        delay(10);
+        if (set > 200)
+        {
+          set = 200;
+        }
+      }
+      else if (!digitalRead(4))
+      {
+        while (!digitalRead(4))
+        {
+        }
+        set -= 10;
+        delay(10);
+        if (set < 150)
+        {
+          set = 150;
+        }
+      }
+      else if (!digitalRead(12))
+      {
+        while (!digitalRead(12))
+        {
+        }
         detik = 0;
-        // lcd.clear();
-        mode = 3;  
+        mode = 1;
+        lcd.clear();
+        delay(10);
+      }
+      if (detik > 2000)
+      {
+        detik = 0;
+      }
     }
-    else
+
+    else if (mode == 1)
     {
+      dmd.drawString(0, 0, String(temp) + " C");
+      lcdPrint(0, 0, "T: " + String(temp) + "C");
+      lcdPrint(2, 1, "Preheat Dulu");
+      setPoint = detik * 1.556; // 140
+      if (temp > 140 && detik < 2500)
+      {
+        lcdPrint(1, 1, "Preheat Selesai");
+        delay(500);
+        lcd.clear();
+        mode = 2;
+      }
       state = true;
     }
-  } 
-  else if (mode == 3 ){
-    state = true;
-    lcdPrint(0, 0, "T: " + String(temp) + "C");
-    if(detik > 10 && detik < 500) {
-      setPoint = set; 
-      if(temp > setPoint){
-        setPoint = 0;
+
+    else if (mode == 2)
+    {
+      dmd.drawString(0, 0, String(temp) + " C");
+      lcdPrint(0, 0, "T: " + String(temp) + "C");
+      lcdPrint(0, 1, "Set to SetPoint");
+      setPoint = set;
+      if (temp > setPoint - 1 && temp < setPoint + 1)
+      {
+        lcdPrint(0, 1, "  SetPoint !!!  ");
+        delay(500);
+        detik = 0;
+        mode = 3;
       }
-    } else if(detik > 600){
+      else
+      {
+        state = true;
+      }
+    }
+    else if (mode == 3)
+    {
+      state = true;
+      dmd.drawString(0, 0, String(temp) + " C");
+      lcdPrint(0, 0, "T: " + String(temp) + "C");
+      if (detik > 10 && detik < 50)
+      {
+        setPoint = set;
+        if (temp > setPoint)
+        {
+          setPoint = 0;
+        }
+      }
+      else if (detik > 55)
+      {
         state = false;
-        lcdPrint(0, 1, "Cooling");
-        if(temp < 50){
-          lcdPrint(0,1, "Cooling Selesai");
+        lcdPrint(0, 1, "Cooling        ");
+        if (temp < 50)
+        {
+          lcdPrint(0, 1, "Cooling Selesai");
           delay(2000);
           lcd.clear();
           detik = 0;
           mode = 0;
         }
-    } 
-    
+      }
+    }
   }
 
   if (state)
   {
-    pidController();
+    plate.setPoint(setPoint);
   }
   else
   {
-    analogWrite(10, 0);
-    analogWrite(9, 0);
+    analogWrite(3, 0);
+    analogWrite(5, 0);
   }
-}
 
-void pidController()
-{ 
-  float PID_RefreshRate = 50;
-  unsigned long prevTime_PID = 0;
-  if(millis() - prevTime_PID >= PID_RefreshRate){
-  
-  PID_ERROR = setPoint - temp;
-  PID_P = kP * PID_ERROR;
-  PID_I = PID_I + (kI * PID_ERROR);
-  PID_D = kD * (PID_ERROR - PREV_ERROR);
-  PID_OUTPUT = PID_P + PID_I + PID_D;
+  if (modePlay == 2)
+  {
+    time();
+    plate.readTemp(temp);
+    lcdPrint(0, 0, "T: " + String(temp) + "C");
+    if(!digitalRead(2)){
+      while(!digitalRead(2)){}
+      lcdPrint(0, 1, "Manual Mode");
+      mode += 1;
+      if(mode > 2){
+        mode = 0;
+      }
+    }
+    if(mode == 1){
+      plate.setPoint(180);
+      lcdPrint(0, 1, "Panas BOSS");
+    } else if(mode == 2){
+      plate.setPoint(0);
+      lcdPrint(0, 1, "Dingin BOSS");
+    }
 
-  if (PID_OUTPUT > PID_OUTPUT_MAX)
-  {
-    PID_OUTPUT = PID_OUTPUT_MAX;
-  }
-  else if (PID_OUTPUT < PID_OUTPUT_MIN)
-  {
-    PID_OUTPUT = PID_OUTPUT_MIN;
-  }
-  pwmVal = PID_OUTPUT;
-  Serial.println(pwmVal);
-  
-  analogWrite(10, pwmVal);
-  analogWrite(9, pwmVal);
-  
-  PREV_ERROR = PID_ERROR;
   }
 }
 
@@ -185,25 +236,24 @@ void lcdPrint(int x, int y, String text)
 {
   lcd.setCursor(x, y);
   lcd.print(text);
-  lcd.print(" ");
+  lcd.print("    ");
 }
 
-void suhu()
+void clearLCDLine(int line)
 {
-  unsigned long prevTime = 0;
-  if (millis() - prevTime >= 100)
+  for (int i = 0; i < 16; i++)
   {
-    temp = therm.analog2temp();
+    lcd.setCursor(i, line);
+    lcd.print(" ");
   }
 }
 
 void time()
 {
   unsigned long prevTime = 0;
-  if (millis() - prevTime > 1000)
+  if (millis() - prevTime >= 1000)
   {
     prevTime = millis();
     detik++;
   }
-  // Serial.println(detik);
 }
